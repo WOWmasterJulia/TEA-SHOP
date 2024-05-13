b83bbc783b5eee9114c69e14abdfd1f4cefde41a
WOWmasterJulia <topazzz@ukr.net>
2024-05-14 00:21:44 +0300

    changes js

--- js/main.js
+++ js/main.js
@@ -16,12 +16,50 @@ menuClose.addEventListener("click", () => {
     menuShadow.classList.remove("menu--open");
 });
 
+// work button
+// document.querySelector('.change-theme').addEventListener('click', function () {
+//     const currentTheme = document.body.className;
+//     if (currentTheme === 'light-theme') {
+//         document.body.className = 'dark-theme';
+//     } else {
+//         document.body.className = "light-theme";
+//     }
+// });
 
-document.querySelector('.change-theme').addEventListener('click', function () {
+
+// Dont Work
+document
+  .querySelector('input[type="checkbox"]')
+  .addEventListener("click", function () {
     const currentTheme = document.body.className;
-    if (currentTheme === 'light-theme') {
-        document.body.className = 'dark-theme';
+    if (currentTheme === "light-theme") {
+      document.body.className = "dark-theme";
     } else {
-        document.body.className = "light-theme";
+      document.body.className = "light-theme";
     }
-});
+  });
+
+
+
+// const checkbox = document.querySelector('input[type="checkbox"]');
+
+// перевіряємо, чи була вже встановлена попередня тема
+// const savedTheme = localStorage.getItem("theme");
+// if (savedTheme) {
+//   document.body.classList.add(savedTheme);
+//     checkbox.checked = savedTheme === "dark";
+    // відмічаємо чекбокс, якщо вибрана темна тема
+// }
+
+// checkbox.addEventListener("change", changeTheme);
+
+// функція змін теми
+// function changeTheme() {
+//   if (checkbox.checked) {
+//     document.body.classList.add("dark");
+//     localStorage.setItem("theme", "dark");
+//   } else {
+//     document.body.classList.remove("dark");
+//     localStorage.setItem("theme", "light");
+//   }
+// }
